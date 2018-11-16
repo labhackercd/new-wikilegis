514b8f28dcbc85dd58c3712ccd31c4fae431604e
erivanio <erivanio.vanin@gmail.com>
2018-11-16 11:41:38 -0200

    update templates structure

--- src/wikilegis/urls.py
+++ src/wikilegis/urls.py
@@ -18,7 +18,7 @@ from django.urls import path
 from django.views.generic import TemplateView
 
 urlpatterns = [
-    path('', TemplateView.as_view(template_name='home.html')),
+    path('', TemplateView.as_view(template_name='pages/home.html')),
     path('admin/', admin.site.urls),
 ]
 
