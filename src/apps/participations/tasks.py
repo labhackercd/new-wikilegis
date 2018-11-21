55d3f76619aaa24fcc426099a360a6ca94f08438
erivanio <erivanio.vanin@gmail.com>
2018-11-21 11:35:56 -0200

    rename files and remove ipdb

--- src/apps/participations/tasks.py
+++ src/apps/participations/tasks.py
@@ -1,11 +1,10 @@
-from apps.notifications import models, views
+from apps.notifications import models, emails
 
 
 def save_invited_email(sender, instance, **kwargs):
-    import ipdb; ipdb.set_trace()
     for participant in instance.thematic_group.participants.all():
         invited_email, created = models.InvitedEmail.objects.get_or_create(
             group=instance, email=participant.email)
         if created:
-            views.send_invite(participant.email, instance.document.title,
-                              invited_email.hash_id)
+            emails.send_invite(participant.email, instance.document.title,
+                               invited_email.hash_id)
