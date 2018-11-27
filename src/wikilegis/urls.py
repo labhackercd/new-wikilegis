9fbb016c97ffba4a017c6550bdaf8b8fb752d066
Petterson Alves <pettalves@gmail.com>
2018-11-27 13:45:50 -0200

    Improve suggestion input

--- src/wikilegis/urls.py
+++ src/wikilegis/urls.py
@@ -7,6 +7,8 @@ from apps.api.urls import router
 urlpatterns = [
     path('', TemplateView.as_view(template_name='pages/home.html'),
          name='home'),
+    path('document/', TemplateView.as_view(template_name='pages/document.html'),
+         name='document'),
     path('admin/', admin.site.urls),
     path('notifications/', include('apps.notifications.urls')),
     path('api/v1/', include(router.urls)),
