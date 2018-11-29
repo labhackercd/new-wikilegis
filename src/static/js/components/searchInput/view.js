7c5180b78f650929cb267b3f7732415f0d574aad
erivanio <erivanio.vanin@gmail.com>
2018-11-29 10:51:31 -0200

    add search with list.js

--- src/static/js/components/searchInput/view.js
+++ src/static/js/components/searchInput/view.js
@@ -4,10 +4,27 @@ var SearchInputView = function() {};
 
 SearchInputView.prototype.initEvents = function() {
   this.searchInputScrollOnClick();
+  this.initSearch();
 };
 
 SearchInputView.prototype.searchInputScrollOnClick = function () {
   $('.js-search').on('click', function() {
-    $([document.documentElement, document.body]).animate({scrollTop: $('.js-search').offset().top - 60}, 200);
+    $([document.documentElement, document.body]).animate(
+      {scrollTop: $('.js-search').offset().top - 60},
+      200
+    );
+  });
+};
+
+SearchInputView.prototype.initSearch = function () {
+  var options = {
+    valueNames: ['js-title', 'js-description'],
+  };
+
+  var listObj = new List('js-projects-list', options);
+
+  $('.js-search').on('keyup', function() {
+    var searchString = $(this).val();
+    listObj.search(searchString);
   });
 };
