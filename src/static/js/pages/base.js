798097397fa482e9b4ef5af2a7b753b657a25c70
Petterson Alves <pettalves@gmail.com>
2018-12-19 10:05:26 -0200

    Fix navbar

--- src/static/js/pages/base.js
+++ src/static/js/pages/base.js
@@ -1,4 +1,7 @@
-/*global GenericModalView */
+/*global GenericModalView NavBarView */
 
 var genericModalView = new GenericModalView();
 genericModalView.initEvents();
+
+var navBarView = new NavBarView();
+navBarView.initEvents();
