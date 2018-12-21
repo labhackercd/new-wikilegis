4073fc223368f9d4938857daf86324ca5c56b366
Matheus Fernandes <matheus.souza.fernandes@gmail.com>
2018-12-21 16:34:33 -0200

    Add new models to persist CD infos
    Signed-off-by: Matheus Fernandes <matheus.souza.fernandes@gmail.com>

--- src/apps/projects/admin.py
+++ src/apps/projects/admin.py
@@ -1,21 +1,21 @@
 from django.contrib import admin
 
-from .models import Theme, DocumentType, Document, Excerpt, ExcerptType
+from . import models
 
 
-@admin.register(Theme)
+@admin.register(models.Theme)
 class ThemeAdmin(admin.ModelAdmin):
     list_display = ('id', 'name', 'slug')
     search_fields = ('name', 'slug')
     prepopulated_fields = {'slug': ['name']}
 
 
-@admin.register(DocumentType)
+@admin.register(models.DocumentType)
 class DocumentTypeAdmin(admin.ModelAdmin):
     list_display = ('id', 'title', 'initials')
 
 
-@admin.register(Document)
+@admin.register(models.Document)
 class DocumentAdmin(admin.ModelAdmin):
     list_display = (
         'id',
@@ -34,7 +34,7 @@ class DocumentAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ['title']}
 
 
-@admin.register(ExcerptType)
+@admin.register(models.ExcerptType)
 class ExcerptTypeAdmin(admin.ModelAdmin):
     list_display = ('id', 'name', 'slug', 'align_center')
     list_filter = ('align_center',)
@@ -42,7 +42,7 @@ class ExcerptTypeAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ['name']}
 
 
-@admin.register(Excerpt)
+@admin.register(models.Excerpt)
 class ExcerptAdmin(admin.ModelAdmin):
     list_display = (
         'id',
@@ -54,3 +54,23 @@ class ExcerptAdmin(admin.ModelAdmin):
         'content',
     )
     list_filter = ('document', 'parent')
+
+
+@admin.register(models.DocumentInfo)
+class DocumentInfoAdmin(admin.ModelAdmin):
+    list_display = (
+        'cd_id',
+        'document',
+        'abridgement',
+        'status',
+    )
+    list_filter = ('document', 'status')
+
+
+@admin.register(models.DocumentAuthor)
+class DocumentAuthorAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'author_type',
+    )
+    list_filter = ('author_type', )
