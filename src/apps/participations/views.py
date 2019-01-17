51f3e2a2b83f2d80352b80646d1fa1bae5b8da23
erivanio <erivanio.vanin@gmail.com>
2019-01-17 10:27:13 -0200

    add participants validation

--- src/apps/participations/views.py
+++ src/apps/participations/views.py
@@ -12,6 +12,7 @@ from apps.participations.models import InvitedGroup, Suggestion, OpinionVote
 from apps.accounts.models import ThematicGroup
 from django.contrib.auth import get_user_model
 from django.urls import reverse_lazy
+from django.forms import ValidationError
 
 User = get_user_model()
 
@@ -43,6 +44,10 @@ class InvitedGroupCreate(CreateView):
         if participants_ids:
             participants = User.objects.filter(id__in=participants_ids)
             thematic_group.participants.set(participants)
+        if not len(participants_ids):
+            form.add_error(None, ValidationError(
+                _('Participants are required')))
+            return super().form_invalid(form)
         self.object.thematic_group = thematic_group
         self.object.save()
         return super().form_valid(form)
