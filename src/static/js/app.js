b8f37adf8c5e2310e1fcd152b1cfc84916733371
Petterson Alves <pettalves@gmail.com>
2019-02-26 10:51:55 -0300

    Remove scroll

--- src/static/js/app.js
+++
@@ -1,9 +0,0 @@
-// if no Webkit browser
-(function(){
-  let isChrome = /Chrome/.test(navigator.userAgent) && /Google Inc/.test(navigator.vendor);
-  let isSafari = /Safari/.test(navigator.userAgent) && /Apple Computer/.test(navigator.vendor);
-  let scrollbarDiv = document.querySelector('.scrollbar');
-    if (!isChrome && !isSafari) {
-      scrollbarDiv.innerHTML = 'You need Webkit browser to run this code';
-    }
-})();
