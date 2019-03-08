5c8d9a4db8d20264917ae7b2b805dca4b3eb334e
Petterson Alves <pettalves@gmail.com>
2019-03-08 12:44:53 -0300

    Create dashboard groups

--- src/apps/projects/urls.py
+++ src/apps/projects/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.views.generic import TemplateView
 from apps.projects import views
 from apps.participations.views import InvitedGroupDetailView
 
@@ -13,5 +14,7 @@ urlpatterns = [
     path('edit/<int:pk>-<slug:documment_slug>/',
          views.DocumentUpdateView.as_view(), name="edit_document"),
     path('document/<int:pk>', views.EditDocumentView.as_view(),
-         name='edit_document')
+         name='edit_document'),
+    path('groups/', TemplateView.as_view(template_name="pages/groups.html", extra_context={'is_owner': True}),
+         name='groups')
 ]
