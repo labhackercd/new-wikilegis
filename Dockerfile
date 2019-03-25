93b9b6d4f307a9a0d0889126ef536fac7beac3c8
erivanio <erivanio.vanin@gmail.com>
2019-03-25 15:04:35 -0300

    fix start.sh

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 FROM labhackercd/alpine-python3-nodejs
 
-ENV BUILD_PACKAGES postgresql-dev postgresql-client gettext
+ENV BUILD_PACKAGES postgresql-dev postgresql-client gettext freetype-dev libpng-dev openblas-dev
 
 RUN apk add --update --no-cache $BUILD_PACKAGES
 RUN mkdir -p /var/labhacker/new-wikilegis
@@ -8,14 +8,18 @@ RUN mkdir -p /var/labhacker/new-wikilegis
 ADD . /var/labhacker/new-wikilegis
 WORKDIR /var/labhacker/new-wikilegis
 
+RUN pip3 install --upgrade pip
 RUN pip3 install -U pipenv psycopg2 gunicorn
 
 RUN pipenv install --system
 
 RUN npm install && \
-    python3 src/manage.py build_mkdocs && \
-    python3 src/manage.py collectstatic --no-input && \
+    npm rebuild node-sass --force
+
+RUN python3 src/manage.py collectstatic --no-input && \
     python3 src/manage.py compilemessages
+
+RUN chmod 755 start.sh
 
 EXPOSE 8000
 CMD ["python3", "src/manage.py", "runserver", "0.0.0.0:8000"]
--- start.sh
+++ start.sh
@@ -11,14 +11,15 @@ done
 
 PGPASSWORD=$DATABASE_PASSWORD psql -U $DATABASE_USER -w -h $DATABASE_HOST -c "CREATE DATABASE ${DATABASE_NAME} OWNER ${DATABASE_USER}"
 
-python3 manage.py migrate
-python3 create_admin.py
-python3 manage.py compress --force
-python3 manage.py collectstatic --no-input
+python3 src/manage.py migrate
+python3 src/create_admin.py
+python3 src/manage.py compress --force
+python3 src/manage.py collectstatic --no-input
+
+NAME="wikilegis"
+[[ -z "${WORKERS}" ]] && NUM_WORKERS=2 || NUM_WORKERS="${WORKERS}"
+DJANGO_WSGI_MODULE=wikilegis.wsgi
 
 # Start your Django Unicorn
 # Programs meant to be run under supervisor should not daemonize themselves (do not use --daemon)
-exec gunicorn ${DJANGO_WSGI_MODULE}:application \
-  --name $NAME \
-  --workers $NUM_WORKERS \
-  --bind=0.0.0.0:8000
+exec gunicorn --chdir src ${DJANGO_WSGI_MODULE}:application --name $NAME --workers $NUM_WORKERS --bind=0.0.0.0:8000
