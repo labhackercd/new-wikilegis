f02d6e9f147ff3125da175de400fd6855420e08d
Matheus Fernandes <matheus.souza.fernandes@gmail.com>
2019-04-29 17:10:56 -0300

    Finished html importer
    Signed-off-by: Matheus Fernandes <matheus.souza.fernandes@gmail.com>

--- src/apps/projects/lexml.py
+++ src/apps/projects/lexml.py
@@ -1,4 +1,5 @@
-from apps.projects import models
+from apps.projects.models import Excerpt, ExcerptType
+from apps.participations.models import Amendment
 from bs4 import BeautifulSoup
 from collections import Counter
 
@@ -25,96 +26,12 @@ RESET_BY_ARTICULATION = {
 ARTICULATION_COUNTER = Counter()
 
 
-def roman_to_int(input):
-    """ Convert a Roman numeral to an integer. """
-    if not isinstance(input, type("")):
-        raise TypeError("expected string, got %s" % type(input))
+def parse_html(html, version, document=None, group=None, author=None):
+    if document is None and group is None:
+        raise Exception(
+            'Missing parameters! You need to provide a Document or a group'
+        )
 
-    input = input.upper()
-    nums = {'M': 1000, 'D': 500, 'C': 100, 'L': 50, 'X': 10, 'V': 5, 'I': 1}
-    sum = 0
-    for i in range(len(input)):
-        try:
-            value = nums[input[i]]
-            # If the next place holds a larger number, this value is negative
-            if i + 1 < len(input) and nums[input[i + 1]] > value:
-                sum -= value
-            else:
-                sum += value
-        except KeyError:
-            raise ValueError('input is not a valid Roman numeral: %s' % input)
-    return sum
-
-
-def create_title(data, parent):
-    number = data['Rotulo']
-    number = number.replace('Título ', '')
-    number = number.replace('Capítulo ', '')
-    number = number.replace('Seção ', '')
-    number = number.replace('Subseção ', '')
-    number = roman_to_int(number.strip())
-    return {
-        'parent': parent,
-        # 'excerpt_type': models.ExcerptType.objects.get(slug='titulo'),
-        'number': number,
-        'content': data['NomeAgrupador'],
-    }
-
-
-def create_excerpt(excerpt_type, data, parent=None, order=0):
-    if excerpt_type == 'Livro':
-        print('Livro')
-        return create_title(data, parent)
-    elif excerpt_type == 'Titulo':
-        print('Titulo')
-        return create_title(data, parent)
-    elif excerpt_type == 'Capitulo':
-        print('Capitulo')
-        return create_title(data, parent)
-    elif excerpt_type == 'Secao':
-        print('Secao')
-        return create_title(data, parent)
-    elif excerpt_type == 'Subsecao':
-        print('Subsecao')
-        return create_title(data, parent)
-    elif excerpt_type == 'Artigo':
-        print('Artigo')
-    elif excerpt_type == 'Paragrafo':
-        print('Paragrafo')
-    elif excerpt_type == 'Inciso':
-        print('Inciso')
-    elif excerpt_type == 'Alinea':
-        print('Alinea')
-    elif excerpt_type == 'Item':
-        print('Item')
-    else:
-        print('Citacao')
-
-
-# def parse_node(excerpt_type, data, parent=None, order=0):
-#     node_data = create_excerpt(excerpt_type, data, parent, order)
-#     print(order)
-#     for key, value in data.items():
-#         if key in ARTICULATIONS:
-#             if type(value) != list:
-#                 value = [value]
-#             for child in value:
-#                 order = parse_node(key, child, order=order + 1)
-
-#             if 'Caput' in data.keys():
-#                 order = parse_node(key, data['Caput'], order=order + 1)
-#     return order
-
-# def parse_lexml(lexml):
-#     data = xmltodict.parse(lexml)['Articulacao']
-#     for key, value in data.items():
-#         if key in ARTICULATIONS:
-#             if type(value) != list:
-#                 value = [value]
-#             for node in value:
-#                 parse_node(key, node)
-
-def parse_html(html, document):
     soup = BeautifulSoup(html, features='html.parser')
     excerpts = soup.findAll('p')
     for order, excerpt in enumerate(excerpts):
@@ -123,13 +40,28 @@ def parse_html(html, document):
         content = excerpt.text
         ARTICULATION_COUNTER.update([excerpt_type])
         print(excerpt_type)
-        models.Excerpt.objects.create(
-            document=document,
-            order=order,
-            excerpt_type=models.ExcerptType.objects.get(slug=excerpt_type),
-            number=number,
-            content=content
-        )
+        if document:
+            Excerpt.objects.create(
+                document=document,
+                order=order,
+                excerpt_type=ExcerptType.objects.get(slug=excerpt_type),
+                number=number,
+                content=content,
+                version=version
+            )
+        elif group:
+            if author is None:
+                raise Exception('Author is mandatory when group is not None')
+
+            Amendment.objects.create(
+                invited_group=group,
+                order=order,
+                excerpt_type=ExcerptType.objects.get(slug=excerpt_type),
+                number=number,
+                content=content,
+                version=version,
+                author=author,
+            )
 
         for resetable in RESET_BY_ARTICULATION[excerpt_type]:
             ARTICULATION_COUNTER[resetable] = 0
