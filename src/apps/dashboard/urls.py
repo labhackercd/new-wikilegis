fb82acf8b30c616d2e67d603b05f1ea0695dfe7e
Petterson Alves <pettalves@gmail.com>
2019-04-30 13:35:41 -0300

    Create project flow

--- src/apps/dashboard/urls.py
+++ src/apps/dashboard/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
 from apps.dashboard import views
+from django.views.generic import TemplateView
 
 
 urlpatterns = [
     path('', views.OwnerDocumentsView.as_view(), name="owner_documents"),
     path('document/<int:pk>', views.EditDocumentView.as_view(),
+         name='edit_document'),
+    path('page', TemplateView.as_view(template_name='pages/closing-date.html'),
          name='edit_document'),
     # path('groups/', views.OwnerGroupsView.as_view(), name='groups')
 ]
