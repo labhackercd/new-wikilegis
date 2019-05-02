bc19e8d2e1c78e40b846e1a43c990e8c8ae87c07
cfviotti <caio@viotti.me>
2019-05-02 18:54:17 -0300

    Fix url paths to take edem paths into consideration

--- src/wikilegis/settings.py
+++ src/wikilegis/settings.py
@@ -87,6 +87,7 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 'wikilegis.processors.settings_variables',
+                'wikilegis.processors.current_url',
             ],
         },
     },
