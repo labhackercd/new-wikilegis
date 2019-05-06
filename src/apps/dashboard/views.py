3ec38dec5e253c4c7a5839035277150addfa653c
erivanio <erivanio.vanin@gmail.com>
2019-05-06 10:11:18 -0300

    add dropdown variables in views

--- src/apps/dashboard/views.py
+++ src/apps/dashboard/views.py
@@ -44,6 +44,10 @@ class DocumentEditorClusterView(DetailView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['is_owner'] = True
+        context['private_groups'] = self.object.invited_groups.filter(
+            public_participation=False)
+        context['public_group'] = self.object.invited_groups.filter(
+            public_participation=True)[:1]
         group_id = self.request.GET.get('group_id', None)
         if self.kwargs['template'] == 'clusters':
             if group_id:
