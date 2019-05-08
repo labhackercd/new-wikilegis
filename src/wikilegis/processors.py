890b6cbf8327f63240821a330e027fd7afd7b836
Caio Viotti <cfviotti@users.noreply.github.com>
2019-05-08 14:01:55 -0300

    Fix url paths to take edem paths into consideration (#106)

--- src/wikilegis/processors.py
+++ src/wikilegis/processors.py
@@ -1,6 +1,20 @@
 from django.contrib.sites.models import Site
+from django.conf import settings
+import re
 
 
 def settings_variables(request):
     site_url = Site.objects.get_current().domain
     return {'site_url': site_url}
+
+
+def current_url(request):
+    if settings.FORCE_SCRIPT_NAME:
+        path = re.sub(
+            r'^{}'.format(settings.FORCE_SCRIPT_NAME),
+            '',
+            request.path
+        )
+    else:
+        path = request.path
+    return {'current_path': path}
