0a4d144cbaeb63dd39282662bcd362953ed42ae1
erivanio <erivanio.vanin@gmail.com>
2019-06-03 16:01:10 -0300

    toggle icons

--- src/static/js/components/documentOpinionsBody/view.js
+++ src/static/js/components/documentOpinionsBody/view.js
@@ -3,9 +3,9 @@
 var DocumentOpinionsBodyView = function() {};
 
 DocumentOpinionsBodyView.prototype.initEvents = function() {
-  this.participantsButton = $('.js-participantsButton');
-  this.opinionsButton = $('.js-opinionsButton');
-  this.votesButton = $('.js-votesButton');
+  this.participantsButton = $('.js-metricsButtons .js-participantsButton');
+  this.opinionsButton = $('.js-metricsButtons .js-opinionsButton');
+  this.votesButton = $('.js-metricsButtons .js-votesButton');
   this.publishers();
 };
 
@@ -16,17 +16,23 @@ DocumentOpinionsBodyView.prototype.publishers = function () {
     $('.js-metricsButtons').children().removeClass('-active');
     self.participantsButton.addClass('-active');
     $('.js-documentExcerpt').removeClass('js-opinionsActive js-votesActive').addClass('js-participationActive');
+    $('.js-metricsIcons').children().hide();
+    $('.js-metricsIcons .js-participantsIcon').show();
   });
 
   self.opinionsButton.on('click', function() {
     $('.js-metricsButtons').children().removeClass('-active');
     self.opinionsButton.addClass('-active');
     $('.js-documentExcerpt').removeClass('js-participationActive js-votesActive').addClass('js-opinionsActive');
+    $('.js-metricsIcons').children().hide();
+    $('.js-metricsIcons .js-opinionsIcon').show();
   });
 
   self.votesButton.on('click', function() {
     $('.js-metricsButtons').children().removeClass('-active');
     self.votesButton.addClass('-active');
     $('.js-documentExcerpt').removeClass('js-opinionsActive js-participationActive').addClass('js-votesActive');
+    $('.js-metricsIcons').children().hide();
+    $(.'js-metricsIcons .js-votesIcon').show();
   });
 };
