f68d62c19be51916db1de77df6722622f57d98da
Petterson Alves <pettalves@gmail.com>
2019-06-04 14:47:53 -0300

    Improve opinions (#174)

--- src/static/js/components/opinionsMetrics/view.js
+++ src/static/js/components/opinionsMetrics/view.js
@@ -25,18 +25,25 @@ OpinionMetricsView.prototype.subscribers = function() {
 };
 
 OpinionMetricsView.prototype.publishers = function() {
+  var self = this;
+
   $('.js-excerptWrapper').on('click', function() {
     if (!$(this).closest('.js-excerptWrapper').hasClass('-enabled')) {
       events.showOpinions.publish($(this).children('.js-documentExcerpt').data('id'));
     }
     events.highlightExcerpt.publish($(this).closest('.js-excerptWrapper'));
   });
-  // $('.js-opinions').on('click .js-suggestionOpinion', function(e) {
-  //   var excerptId = $(e.target).closest('.js-suggestionOpinion').data('excerptId');
-  //   var currentId = $(e.target).closest('.js-suggestionOpinion').data('opinionId');
-  //   var excerpt = $(`.js-documentExcerpt[data-id='${excerptId}']`);
-  //   events.openHighlightTooltip.publish(excerpt, currentId.toString());
-  // });
+
+  $('.js-documentEditor').on('click', function(e) {
+    if (!$(e.target).hasClass('js-metricsIcons') &&
+        $(e.target).closest('.js-excerptWrapper').length === 0 &&
+        $(e.target).closest('.js-sideBar').length === 0 &&
+        $(e.target).closest('.js-toolBar').length === 0 &&
+        $(e.target).closest('.js-metricsButtons').length === 0
+      ) {
+        self.resetOpinions();
+    }
+  });
 };
 
 OpinionMetricsView.prototype.get_opinions = function(excerptId=undefined) {
@@ -54,40 +61,62 @@ OpinionMetricsView.prototype.get_opinions = function(excerptId=undefined) {
 
   request.done(function(data) {
     self.opinionsElement.html(data.opinionsHtml);
+
     $('.js-opinions .js-suggestionOpinion').on('click', function(e) {
-      events.selectSuggestion.publish(e.target.closest('.js-suggestionOpinion'));
+      var suggestionOpinion = $(e.target).closest('.js-suggestionOpinion');
+      events.selectSuggestion.publish(suggestionOpinion);
     });
+
+    $('.js-opinions .js-suggestionOpinion').on('mouseenter', function(e) {
+      var suggestionOpinion = $(e.target).closest('.js-suggestionOpinion');
+      var excerptId = $(suggestionOpinion).data('excerptId');
+      var currentId = $(suggestionOpinion).data('opinionId');
+      var excerpt = $(`.js-documentExcerpt[data-id='${excerptId}']`);
+
+      events.openHighlightTooltip.publish(excerpt, currentId.toString());
+    });
+
+    $('.js-opinions .js-suggestionOpinion').on('mouseleave', function(e) {
+      events.closeHighlightTooltip.publish($('.js-documentExcerpt'));
+    });
+
+    $('.js-suggestionText').truncate({lines: 3});
   });
 };
 
 OpinionMetricsView.prototype.select = function (suggestionOpinion) {
   if ($(suggestionOpinion).hasClass('-active')) {
-    events.closeHighlightTooltip.publish($('.js-documentExcerpt'));
-    $('.js-opinions .js-suggestionOpinion').removeClass('-active');
+    suggestionOpinion.removeClass('-active');
+
+    suggestionOpinion.find('.js-suggestionText').truncate('collapse')
 
   } else {
-    events.closeHighlightTooltip.publish($('.js-documentExcerpt'));
     $('.js-opinions .js-suggestionOpinion').removeClass('-active');
 
     $(suggestionOpinion).addClass('-active');
 
-    var excerptId = $(suggestionOpinion).data('excerptId');
-    var currentId = $(suggestionOpinion).data('opinionId');
-    var excerpt = $(`.js-documentExcerpt[data-id='${excerptId}']`);
-    events.openHighlightTooltip.publish(excerpt, currentId.toString());
+    suggestionOpinion.find('.js-suggestionText').truncate('expand');
   }
 };
 
+OpinionMetricsView.prototype.resetOpinions = function () {
+  var self = this;
+  
+  $('.js-documentEditor').removeClass('-supressed');
+  $('.js-excerptWrapper').removeClass('-enabled');
+  self.opinionsElement.html(`
+    <div class="empty">
+      Selecione um trecho ao lado para ver as opiniões.
+    </div>
+  `);
+};
+
 OpinionMetricsView.prototype.highlightExcerpt = function (excerptElement) {
+  var self = this;
+
   events.closeHighlightTooltip.publish($('.js-documentExcerpt'));
   if (excerptElement.hasClass('-enabled')) {
-    $('.js-documentEditor').removeClass('-supressed');
-    $('.js-excerptWrapper').removeClass('-enabled');
-    this.opinionsElement.html(`
-      <div class="empty">
-        Selecione um trecho ao lado para ver as opiniões.
-      </div>
-    `);
+    self.resetOpinions();
   } else {
     $('.js-excerptWrapper').removeClass('-enabled');
     $('.js-documentEditor').addClass('-supressed');
