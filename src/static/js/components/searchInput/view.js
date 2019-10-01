5a7a1238cbdbe7c466f03bccf4e366e68b7d0aa8
Thiago Nogueira <thiagonf10@gmail.com>
2019-10-01 10:41:32 -0300

    Fixing lint errors

--- src/static/js/components/searchInput/view.js
+++ src/static/js/components/searchInput/view.js
@@ -1,16 +1,16 @@
 /*global $ List*/
 
-var SearchInputView = function() {};
+var SearchInputView = function () { };
 
-SearchInputView.prototype.initEvents = function() {
+SearchInputView.prototype.initEvents = function () {
   this.searchInputScrollOnClick();
   this.initSearch();
 };
 
 SearchInputView.prototype.searchInputScrollOnClick = function () {
-  $('.js-search').on('click', function() {
+  $('.js-search').on('click', function () {
     $([document.documentElement, document.body]).animate(
-      {scrollTop: $('.js-search').offset().top - 80},
+      { scrollTop: $('.js-search').offset().top - 80 },
       200
     );
   });
@@ -23,14 +23,14 @@ SearchInputView.prototype.initSearch = function () {
 
   var listObj = new List('js-projects-list', options);
 
-  $('.js-search').on('keyup', function() {
+  $('.js-search').on('keyup', function () {
     var searchString = $(this).val();
     listObj.search(searchString);
 
     if (listObj.matchingItems.length > 0) {
-      $('.no-result').hide()
+      $('.no-result').hide();
     } else {
-      $('.no-result').show()
+      $('.no-result').show();
     }
   });
 };
