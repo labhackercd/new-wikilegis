2e67293025801944657d5ba1f479b390b6a719eb
Teogenes Moura <teogenesmoura@Teogeness-MacBook-Pro.local>
2019-12-09 17:25:50 -0200

    fixing lint

--- src/static/js/components/switchTexts/view.js
+++ src/static/js/components/switchTexts/view.js
@@ -1,28 +1,28 @@
-/* global $ events */
+/*globals $:false */
 
 var SwitchTexts = function() {};
 
 SwitchTexts.prototype.initEvents = function() {
-	this.original = $('.diff .original');
-	this.diferencas = $('.diff .diferencas');
-	this.final = $('.diff .final');
-	let obj = { "diffText": "Texto <span class='deleted'>de diferencas </span><span class='added'>entre as</span> versoes",
-		"originalText":  "Texto original sem diferencas",
-		"finalText": "Texto final com alteracoes populares" };
-	this.alternateTexts(obj);
-}
+  this.original = $('.diff .original');
+  this.diferencas = $('.diff .diferencas');
+  this.final = $('.diff .final');
+  let obj = { 'diffText': 'Texto <span class="deleted">de diferencas </span><span class="added">entre as</span> versoes',
+    'originalText':   'Texto original sem diferencas',
+    'finalText': 'Texto final com alteracoes populares' };
+  this.alternateTexts(obj);
+};
 
-SwitchTexts.prototype.alternateTexts = function(obj) {
-	this.original.on("click", function(texto) {
-		$('.diff .texto').html(obj.originalText);
-	});
-	this.diferencas.on("click", function(texto) {
-		$('.diff .texto').html(obj.diffText);
-	});
-	this.final.on("click", function(texto) {
-		$('.diff .texto').text(obj.finalText);
-	});
-	$(document).ready(function(texto) {
-		$('.diff .texto').html(obj.diffText);
-	});
-}
+SwitchTexts.prototype.alternateTexts = function(obj) { // eslint-disable-line no-unused-vars
+  this.original.on('click', function(obj) {
+    $('.diff .texto').html(obj.originalText);
+  });
+  this.diferencas.on('click', function(obj) {
+    $('.diff .texto').html(obj.diffText);
+  });
+  this.final.on('click', function(obj) {
+    $('.diff .texto').text(obj.finalText);
+  });
+  $(document).ready(function(obj) {
+    $('.diff .texto').html(obj.diffText);
+  });
+};
