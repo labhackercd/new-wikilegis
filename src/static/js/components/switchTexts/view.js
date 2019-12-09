8123e29247097ddbd8be7cfdef04e531501a0744
Teogenes Moura <teogenesmoura@Teogeness-MacBook-Pro.local>
2019-12-09 14:30:04 -0200

    implemented diff screen between original, modified and final versions of a text

--- src/static/js/components/switchTexts/view.js
+++ src/static/js/components/switchTexts/view.js
@@ -6,7 +6,7 @@ SwitchTexts.prototype.initEvents = function() {
 	this.original = $('.diff .original');
 	this.diferencas = $('.diff .diferencas');
 	this.final = $('.diff .final');
-	let obj = { "diffText": "Texto de diferencas entre as versoes",
+	let obj = { "diffText": "Texto <span class='deleted'>de diferencas </span><span class='added'>entre as</span> versoes",
 		"originalText":  "Texto original sem diferencas",
 		"finalText": "Texto final com alteracoes populares" };
 	this.alternateTexts(obj);
@@ -14,15 +14,15 @@ SwitchTexts.prototype.initEvents = function() {
 
 SwitchTexts.prototype.alternateTexts = function(obj) {
 	this.original.on("click", function(texto) {
-		$('.diff .texto').text(obj.originalText);
+		$('.diff .texto').html(obj.originalText);
 	});
 	this.diferencas.on("click", function(texto) {
-		$('.diff .texto').text(obj.diffText);
+		$('.diff .texto').html(obj.diffText);
 	});
 	this.final.on("click", function(texto) {
 		$('.diff .texto').text(obj.finalText);
 	});
 	$(document).ready(function(texto) {
-		$('.diff .texto').text(obj.diffText);
+		$('.diff .texto').html(obj.diffText);
 	});
 }
