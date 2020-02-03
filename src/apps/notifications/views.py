4ffa7b90e913f92f1b3dd0c0afb12a04c9848a58
Thiago Nogueira <thiagonf10@gmail.com>
2020-02-03 10:52:08 -0300

    Fixing flake8

--- src/apps/notifications/views.py
+++ src/apps/notifications/views.py
@@ -1,4 +1,3 @@
-from django.core.exceptions import PermissionDenied
 from apps.notifications.models import (ParcipantInvitation,
                                        PublicAuthorization, Notification,
                                        FeedbackAuthorization)
