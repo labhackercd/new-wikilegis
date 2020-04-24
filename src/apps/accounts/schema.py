f3ebacdf30049a0094a7d8f0874939bd0128be06
Thiago Nogueira <thiagonf10@gmail.com>
2020-04-24 15:33:55 -0300

    Fixing search by usearname on graphql

--- src/apps/accounts/schema.py
+++ src/apps/accounts/schema.py
@@ -7,7 +7,7 @@ from apps.accounts.models import UserProfile
 class UserType(DjangoObjectType):
     class Meta:
         model = User
-        exclude = ('username', 'email', 'is_superuser', 'is_staff',
+        exclude = ('email', 'is_superuser', 'is_staff',
                    'is_active', 'password')
 
 
@@ -23,7 +23,7 @@ class Query(graphene.ObjectType):
     profiles = graphene.List(UserProfileType)
     user = graphene.Field(UserType,
                           id=graphene.Int(),
-                          firstName=graphene.String())
+                          username=graphene.String())
 
     def resolve_users(self, info, **kwargs):
         return User.objects.filter(is_active=True)
@@ -33,12 +33,12 @@ class Query(graphene.ObjectType):
 
     def resolve_user(self, info, **kwargs):
         id = kwargs.get('id')
-        name = kwargs.get('firstName')
+        username = kwargs.get('username')
 
         if id is not None:
             return User.objects.get(pk=id)
 
-        if name is not None:
-            return User.objects.get(first_name=name)
+        if username is not None:
+            return User.objects.get(username=username)
 
         return None
