278edfdf3ebb1e4699391711cccdade3b43a69c9
Teogenes Moura <teogenesmoura@Teogeness-MacBook-Pro.local>
2020-05-13 10:33:12 -0300

    fixing linting errors

--- src/static/js/components/documentCard/view.js
+++ src/static/js/components/documentCard/view.js
@@ -3,20 +3,20 @@
 var DocumentCardView = function() {};
 
 DocumentCardView.prototype.initEvents = function() {
-	this.documentCard();
-}
+  this.documentCard();
+};
 
 DocumentCardView.prototype.subscriber = function() {
-	var self = this;
-	events.documentCard.subscribe(function(){
-		self.documentCard();
-	});
-}
+  var self = this;
+  events.documentCard.subscribe(function(){
+    self.documentCard();
+  });
+};
 
 DocumentCardView.prototype.documentCard = function() {
-	$('.js-description').each(function(x) {
+  $('.js-description').each(function() {
     var currentText = $(this).text();
     var newText = currentText.substring(0, 300) + '...' + '<a class="pseudolink"> Veja mais! </a>';
     $(this).html(newText);
   });		
-}
+};
