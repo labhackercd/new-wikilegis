6151e817010a23596eace5cb7b559f42c04151e8
erivanio <erivanio.vanin@gmail.com>
2021-08-10 11:03:23 -0300

    create endpoint to return registred users #355

--- src/apps/accounts/urls.py
+++ src/apps/accounts/urls.py
@@ -4,4 +4,6 @@ from apps.accounts import views
 urlpatterns = [
     path('participants/autocomplete/',
          views.participants_autocomplete, name='participants_autocomplete'),
+    path('participants/email-list/',
+         views.email_list_participants, name='email_list_participants'),
 ]
--- src/apps/accounts/views.py
+++ src/apps/accounts/views.py
@@ -58,3 +58,29 @@ def participants_autocomplete(request):
         result.append(obj)
 
     return JsonResponse(result, safe=False)
+
+
+def email_list_participants(request):
+    emails = request.GET.get('emails', None)
+    result = []
+
+    for email in emails.replace(';', ',').split(','):
+        if User.objects.filter(email=email).exists():
+            user = User.objects.get(email=email)
+            user_object = {
+                'id': user.id,
+                'first_name': user.first_name,
+                'last_name': user.last_name,
+                'avatar': user.profile.avatar,
+                'themes': [
+                    {'name': theme.name, 'color': theme.color}
+                    for theme in user.profile.themes.all()
+                ],
+            }
+        else:
+            user_object = {
+                'email': email
+            }
+        result.append(user_object)
+
+    return JsonResponse(result, safe=False)
