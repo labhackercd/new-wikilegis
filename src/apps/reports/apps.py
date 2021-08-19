66d58b5596ea3be6650dfe500b35bdb8dcb91254
erivanio <erivanio.vanin@gmail.com>
2021-08-19 16:56:46 -0300

    fix django.po messages

--- src/apps/reports/apps.py
+++ src/apps/reports/apps.py
@@ -1,5 +1,7 @@
 from django.apps import AppConfig
+from django.utils.translation import ugettext_lazy as _
 
 
 class ReportsConfig(AppConfig):
-    name = 'reports'
+    name = 'apps.reports'
+    verbose_name = _('Reports')
